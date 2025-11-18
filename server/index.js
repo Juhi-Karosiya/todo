27aacee4dffb80fed062fa6ffa59dc184f6df140
Juhi-Karosiya <karosiyajuhi@gmail.com>
2025-11-18 20:30:40 +0530

    api added

---
+++ server/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+const allowedOrigins = [
+  "https://to-do-application-qnhv.onrender.com",
+  "http://localhost:5173"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
+app.use(express.json());
+
+const TODO_ITEMS = [
+  {
+    id: 1,
+    todoItem: "Buy Groceries",
+    priority: "high",
+    emoji: "⚖️",
+    isDone: false,
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: 2,
+    todoItem: "Go To Gym",
+    priority: "medium",
+    emoji: "💪",
+    isDone: true,
+    createdAt: new Date().toISOString(),
+  },
+];
