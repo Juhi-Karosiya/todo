c9c6b212574f0e5edb524dcf5666c8a0912923b3
Juhi-Karosiya <karosiyajuhi@gmail.com>
2025-12-01 21:22:37 +0530

    cchanges afdded

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:8080" 
+    ? axios.get("http://localhost:8080/todos")
     : "https://to-do-application-api.onrender.com"; 
 
 export default API_BASE_URL;
