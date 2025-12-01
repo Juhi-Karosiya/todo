3b9f0bcb952e15e13d9d27b5adb87d66cf11f36d
Juhi-Karosiya <karosiyajuhi@gmail.com>
2025-12-01 21:59:52 +0530

    update

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ const app = express();
 
 const allowedOrigins = [
  "https://todo-frnt.onrender.com",
-  "http://localhost:8080"
+   "http://localhost:5173",
+  "http://localhost:3000"
 ];
 
 app.use(cors({
